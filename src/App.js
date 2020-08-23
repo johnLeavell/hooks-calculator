d9cccc545d1669d34ce6afb7c0728c4f131c0f85
John Leavell <johnleavell@gmail.com>
2020-08-22 22:31:07 -0500

    imported useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
+import {v4 as uuidv4 } from 'uuid';
 
+const initialExpenses = [
+  {id: uuidv4(), charge: 'rent', amount: 1600},
+  {id: uuidv4(), charge: 'car payment', amount: 400},
+  {id: uuidv4(), charge: 'credit card', amount: 1200}
+];
+// console.log(initialExpenses);
 function App() {
+  // console.log(useState());
+  const [ expenses, setExpenses ] = useState(initialExpenses);
+  console.log(expenses);
   return (
     <>
     <Alert />
@@ -15,3 +25,11 @@ function App() {
 }
 
 export default App;
+
+
+// implementing state in functional components
+// 1st) import useState()
+// useState() returns an [] with two values
+//  first value: the actual value of the state
+//  second value: function that updates/controls the state
+// pass default in useState()
